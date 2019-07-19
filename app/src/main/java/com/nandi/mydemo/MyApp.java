538c6fc351285c054c1eae962bd95f4739ce9a30
qingsong <email>
2019-07-19 11:26:41 +0800

    Initial commit

---
+++ app/src/main/java/com/nandi/mydemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.nandi.mydemo;
+
+import com.nandi.lqsbaselibrary.base.BaseApplication;
+
+/**
+ * Date:2019-07-19
+ * Time:10:16
+ * author:qingsong
+ */
+public class MyApp extends BaseApplication {
+}
---
+++ lqsbaselibrary/src/main/java/com/nandi/lqsbaselibrary/net/OkgoHelper.java
@@ -0,0 +1,9 @@
+package com.nandi.lqsbaselibrary.net;
+
+/**
+ * Date:2019-07-13
+ * Time:11:26
+ * author:qingsong
+ */
+public class OkgoHelper {
+}
